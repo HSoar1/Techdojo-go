6d7991906b8f9912961bb0955ca51aad76fe9efa
soar1 <soarshizu@gmail.com>
2020-06-29 15:07:08 +0900

    fix

--- mysql/db/init.sql
+++ mysql/db/init.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE gameapi_db;
-use gameapi_db;
+CREATE DATABASE api_database;
+use api_database;
 
-CREATE TABLE user (
+CREATE TABLE users (
     id integer(10), 
     username varchar(255), 
     token varchar(255)); 
 
 
-INSERT INTO user (id, username, token) VALUES ( 1, 'kawauchiya' , 1);
+INSERT INTO users (id, username, token) VALUES ( 1, 'soar' , 'soarstoken');
--- src/main.go
+++ src/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main(){
-	fmt.Printf("Hello World\n")
+	db, err := sql.Open("mysql", "root:@/dojo_api_1")
+	if err != nil{
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil{
+		panic(err.Error())
+	}
+
+	values := make([]sql.RawBytes, len(columns))
+
+	scanArgs := make([]interface{}, len(values))
+	for i := range values{
+		scanArgs[i] = &values[i];
+	}
+
+	for rows.Next(){
+		err = rows.Scan(ScanArgs...)
+		if err != nil{
+			panic(err.Error())
+		}
+
+		var value String
+		for i, col := range values{
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			fmt.Println(columns[i], ": ", value)
+		}
+		fmt.Println("----------------------------------------")
+	}
 }
